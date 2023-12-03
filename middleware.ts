8ddf884761662813aaf92e69916108a1205e121e
Ariel Llanita <llanitaariel@gmail.com>
2023-12-03 13:53:48 +0800

    middleware update for admin

--- middleware.ts
+++ middleware.ts
@@ -1,38 +1,17 @@
-// import { NextResponse, type NextRequest } from "next/server";
-// import { verifyJWT } from "./lib/jwt";
-// import userModel from "./db/models/user.model";
+import { withAuth } from "next-auth/middleware";
 
-// export async function middleware(request: NextRequest) {
-//   if (request.nextUrl.pathname.startsWith("/dashboard")) {
-//     const authError = function (type: string, msg: string) {
-//       return NextResponse.redirect(new URL(`/auth/signin?alert=${type}&msg=${msg}`, request.url));
-//     };
+export default withAuth({
+  callbacks: {
+    authorized({ req, token }) {
+      if (req.nextUrl.pathname.startsWith("/admin")) {
+        return (<any>token?.user)?.role === "admin";
+      }
 
-//     if (request.cookies.has("token")) {
-//       const token = request.cookies.get("token")!.value;
-//       const parsedToken = await verifyJWT(token, process.env.JWT_SECRET_KEY!);
-
-//       if (parsedToken?.id) {
-//         const user = await userModel.findById(parsedToken.id);
-
-//         if (user) {
-//           // TODO: ADD USER TO SESSION
-          
-//           return NextResponse.next();
-//         } else {
-//           authError("error", "Invalid token");
-//         }
-//       } else {
-//         authError("error", "Invalid token");
-//       }
-//     } else {
-//       authError("error", "Missing token");
-//     }
-//   }
-// }
-
-export { default } from "next-auth/middleware";
+      return Boolean(token);
+    },
+  },
+});
 
 export const config = {
-  matcher: ["/dashboard/:path*"],
+  matcher: ["/dashboard/:path*", "/admin/:path*"],
 };
