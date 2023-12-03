c447340acd466813968a6c1522952d28bb2b29e4
Ariel Llanita <llanitaariel@gmail.com>
2023-12-04 00:01:25 +0800

    add prod env

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/Navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  // add
   return <Navbar menuLinks={[{ link: "/api/auth/signout?callbackUrl=/", label: "Logout" }]}>{children}</Navbar>;
 }
