13649d37da54da8c8afdcb1f97ba0a469d3cd68e
Ariel Llanita <llanitaariel@gmail.com>
2023-12-04 08:45:35 +0800

    donation info

--- db/models/animal.model.ts
+++ db/models/animal.model.ts
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 export interface Animal extends mongoose.Document {
   id: string;
@@ -9,19 +9,25 @@ export interface Animal extends mongoose.Document {
   gender: string;
   dateOfEuthanization: Date;
   photo: string;
+  donationInfo: {
+    type: string;
+    accountNumer: string;
+    accountHolderName?: string;
+  };
 }
 
 const schema = new mongoose.Schema<Animal>(
   {
-    type: { type: String, default: 'Dog' },
+    type: { type: String, default: "Dog" },
     name: { type: String, required: true },
     disablities: { type: String, required: true },
     breed: { type: String, required: true },
     gender: { type: String, required: true },
     dateOfEuthanization: { type: Date, required: true },
     photo: { type: String, required: true },
+    donationInfo: { type: Object, required: false },
   },
   { timestamps: true }
 );
 
-export default mongoose.models.Animal || mongoose.model<Animal>('Animal', schema);
+export default mongoose.models.Animal || mongoose.model<Animal>("Animal", schema);
