6ca93f9a82390967aa2e05901a4b55a2cbc3def9
Lizlie <precyrose1998@gmail.com>
2023-12-04 11:43:51 +0800

    modified login

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -11,69 +11,77 @@ function Signin({}: Props) {
   const searchParams = useSearchParams();
 
   return (
-    <div className="md:w-4/5 mx-auto grid grid-cols-2 gap-4 place-content-center h-screen">
-      <div>
-        <p className="text-center">LOGO HERE</p>
-      </div>
+    <div className="container mx-auto flex justify-center items-center h-screen">
+      <div className="w-full max-w-md border rounded-lg p-8 bg-white">
+        <div className="text-center mb-6">
+          <p>LOGO HERE</p>
+        </div>
+        <div>
+          {searchParams.get("error") == "CredentialsSignin" && (
+            <div role="alert" className="alert alert-error mb-4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="stroke-current shrink-0 h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+              <span>Invalid credentials</span>
+            </div>
+          )}
+          <div className="flex flex-col gap-4">
+            <input
+              type="text"
+              name="username"
+              placeholder="Email"
+              value={formData.username}
+              onChange={(e) => setFormData({ ...formData, username: e.target.value })}
+              className="input input-bordered w-full"
+              required
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={formData.password}
+              onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+              className="input input-bordered w-full"
+              required
+            />
 
-      <div>
-        {searchParams.get("error") == "CredentialsSignin" && (
-          <div role="alert" className="alert alert-error mb-4">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="stroke-current shrink-0 h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
+            <button
+              type="submit"
+              className="btn btn-primary btn-block"
+              style={{
+                backgroundColor: "#E0915F",
+                color: "#ffffff",
+                border: "1px solid #E0915F",
+              }}
+              onClick={async () => {
+                signIn("credentials", {
+                  callbackUrl: "/dashboard/home",
+                  ...formData,
+                });
+              }}
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
-            <span>Invalid credentials</span>
+              Login
+            </button>
+
+            <div className="divider">or</div>
+            <Link
+              href={"/auth/signup"}
+              className="btn btn-secondary btn-block"
+              style={{ backgroundColor: "#66564B", color: "#ffffff", border: "1px solid #F4C8A9" }}
+            >
+              Sign up
+            </Link>
           </div>
-        )}
-
-        <div className="flex flex-col gap-4">
-          <input
-            type="text"
-            name="username"
-            placeholder="Email"
-            value={formData.username}
-            onChange={(e) => setFormData({ ...formData, username: e.target.value })}
-            className="input input-bordered w-full"
-            required
-          />
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            value={formData.password}
-            onChange={(e) => setFormData({ ...formData, password: e.target.value })}
-            className="input input-bordered w-full"
-            required
-          />
-
-          <button
-            type="submit"
-            className="btn btn-primary btn-block"
-            onClick={async () => {
-              signIn("credentials", {
-                callbackUrl: '/dashboard/home',
-                ...formData,
-              });
-            }}
-          >
-            Login
-          </button>
-
-          <div className="divider">or</div>
-
-          <Link href={"/auth/signup"} className="btn btn-secondary btn-block">
-            Signup
-          </Link>
         </div>
       </div>
     </div>
--- app/dashboard/[type]/page.tsx
+++ app/dashboard/[type]/page.tsx
@@ -1,42 +1,69 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import React from "react";
 import Image from "next/image";
+import Navbar from "@/components/Navbar";
+
+// Navbar Component
+// const Navbar = () => {
+//   // Add your Navbar content and structure here
+//   return (
+//     <nav className="bg-gray-800 p-4 text-white">
+//       <div className="container mx-auto">
+//         {/* Navbar content goes here */}
+//         Navbar Content
+//       </div>
+//     </nav>
+//   );
+// };
 
 type Props = {
   params: { type: "home" | "favorites" };
 };
 
 async function Page({ params }: Props) {
-  return (
-    <div style={{ display: "flex", height: "100vh" }}>
-      {/* Sidebar */}
-      <div
-        style={{
-          width: "20%",
-          backgroundColor: "#f0f0f0",
-          padding: "20px",
-        }}
-      >
-        Like List
-      </div>
+  const session: any = await getServerSession(authOptions);
+  if (session.user.role === "admin") redirect("/admin/home");
 
-      <div style={{ width: "80%", padding: "20px" }}>
+  return (
+    <div>
+      <Navbar menuLinks={[{ link: "/api/auth/signout?callbackUrl=/", label: "Logout" }]}>
+        {/* {children} */}
+      </Navbar>
+
+      <div style={{ display: "flex", height: "100vh" }}>
+        {/* Sidebar */}
         <div
           style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100%",
+            width: "20%",
+            backgroundColor: "#f0f0f0",
+            padding: "20px",
           }}
         >
-          <div style={{ marginBottom: "20px" }}>
-            {" "}
-            <Image src="/dog1.jpeg" alt="Dashboard Image" width={300} height={200} />
-            {/* <Image src="/dog2.jpeg" alt="Dashboard Image" width={300} height={200} />
-            <Image src="/dog3.jpeg" alt="Dashboard Image" width={300} height={200} /> */}
+          Like List
+        </div>
+
+        <div style={{ width: "80%", padding: "20px" }}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100%",
+            }}
+          >
+            <div style={{ marginBottom: "20px" }}>
+              {" "}
+              <Image src="/dog1.jpeg" alt="Dashboard Image" width={300} height={200} />
+              {/* <Image src="/dog2.jpeg" alt="Dashboard Image" width={300} height={200} />
+              <Image src="/dog3.jpeg" alt="Dashboard Image" width={300} height={200} /> */}
+            </div>
+            <button type="submit" className="btn btn-block" style={{ width: "150px" }}>
+              DONATE
+            </button>
           </div>
-          <button type="submit" className="btn  btn-block" style={{ width: "150px" }}>
-            DONATE
-          </button>
         </div>
       </div>
     </div>
